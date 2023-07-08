74c3af1ba1aabcdfdd71d65fd4c737c08df50ab3
Reagan Bohan <xbjfk.github@gmail.com>
2023-07-08 08:51:42 +0000

    Add region selector in signup.

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import regions from "$lib/data/regions"
+</script>
+
 <form method="POST"> 
     <label>
         email
@@ -12,6 +16,16 @@
     <label>
         confirm password
         <input name="confirmPassword" type="password">
+    </label>
+
+    <label>
+        region
+        <select name="region">
+            <option disabled selected value></option>
+            {#each Object.entries(regions) as [value, {name, maori_name}] }
+                <option value={value}>{name} ({maori_name})</option>
+            {/each}
+        </select>
     </label>
 
     <button> sign up </button>
