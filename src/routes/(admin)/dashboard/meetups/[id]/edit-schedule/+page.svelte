32ed5359306b1a28ac2c227eb0e27f518654c015
Tim Xie <pdt.xie@gmail.com>
2023-07-24 20:14:50 +1200

    conquering thy calendar

--- src/routes/(admin)/dashboard/meetups/[id]/edit-schedule/+page.svelte
+++ src/routes/(admin)/dashboard/meetups/[id]/edit-schedule/+page.svelte
@@ -1,24 +1,37 @@
-<script>
+<script lang="ts">
     import Calendar from "@event-calendar/core";
     import Interaction from "@event-calendar/interaction";
     import TimeGrid from "@event-calendar/time-grid";
 
+    let events = [{
+        "title": "new event",
+        "start": "2023-07-23T18:30:00.000Z",
+        "end": "2023-07-24T05:00:00.000Z"
+    }];
+
     let plugins = [TimeGrid, Interaction];
 
-    let start = new Date();
-    start.setTime(1690123605);
-
-    let end = new Date();
-    end.setTime(1690130805);
-
-    let options = {
+    let options;
+    let evcal;
+    
+    $: options = {
+        events: events,
         view: "timeGridDay",
         allDaySlot: false,
-        events: [
-            { id: 0, start: start, end: end, title: "event", edtiable: true, display: "auto" }
-        ]
+        header: {
+            start: "",
+            center: "title",
+            end: ""
+        },
+        select: (info) => {
+            evcal.addEvent({
+                title: "new event",
+                start: info.start,
+                end: info.end
+            })
+        },
+        selectable: true,
     }
 </script>
 
-<Calendar {plugins} {options} />
-
+<Calendar bind:this={evcal} {plugins} {options} />
