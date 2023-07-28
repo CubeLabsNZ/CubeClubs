9866903eae7cb47c0ac3d33680f713b7d4e81401
Reagan Bohan <xbjfk.github@gmail.com>
2023-07-28 18:03:55 +0300

    register support

---
+++ src/routes/(app)/meetups/[id]/register/+page.server.ts
@@ -0,0 +1,38 @@
+import prisma from '$lib/prisma';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { getUserSessionOrThrow } from '$lib/utilsServer';
+
+export const load = (async ({ params, cookies }) => {
+    const user = await getUserSessionOrThrow(cookies, false)
+
+    const id = Number(params.id)
+    if (isNaN(id)) {
+        throw error(404, 'not found');
+    }
+
+    // TODO: nice http error if already registered
+
+    const userInMeetup = await prisma.meetup.update({
+        where: { id },
+        data: {
+            users: {
+                create: {
+                    user: {
+                        connect: {
+                            id: user.id
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    if (!userInMeetup) {
+        throw error(404, 'not found');
+    }
+
+    return {
+        
+    }
+}) satisfies PageServerLoad
