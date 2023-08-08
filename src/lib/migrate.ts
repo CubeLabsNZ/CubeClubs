a3c8d210e0e40ac21fb532443853c419483cfa76
Tim Xie <pdt.xie@gmail.com>
2023-08-08 16:09:17 +1200

    remove migrate file

--- src/lib/migrate.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "$lib/prisma";
-
-import * as clubs from "../../temp/club.json";
-
-export function importData() {
-    for (const club of clubs.default) {
-        console.log(club);
-    }
-}
