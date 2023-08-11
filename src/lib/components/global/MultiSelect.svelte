4a62bbfd507f57acd3277c1da526b7815b912ef3
Tim Xie <pdt.xie@gmail.com>
2023-08-11 21:46:38 +1200

    add multiselect

---
+++ src/lib/components/global/MultiSelect.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+    export let selectedIndices = new Set();
+
+    export let padding = 12;
+
+    export let labels: Label[];
+
+    export let fixedHeight = true;
+</script>
+
+<script context="module" lang="ts">
+    export interface Label {
+        type: LabelType;
+        data: string;
+    }
+
+    export enum LabelType {
+        Text,
+        Image
+    }
+</script>
+
+
+<div class="container" style:--p={padding}px style:height={fixedHeight ? "34px" : "auto"} style:flex-wrap={fixedHeight ? "nowrap" : "wrap"}>
+    {#each labels as { type, data, }, i}
+        <button on:click|preventDefault = { () => { 
+            if (selectedIndices.has(i)) {
+                selectedIndices.delete(i)
+            } else {
+                selectedIndices.add(i)
+            }
+
+            selectedIndices = selectedIndices
+        }} data-selected={selectedIndices.has(i)}>
+            {#if type == LabelType.Text}
+                <p class="inner">{data}</p>
+            {:else}
+                <img class="inner" src={data} alt={data} height=28px/>
+            {/if}
+        </button>
+    {/each}
+
+    <slot/>
+</div>
+
+
+<style>
+    /* TODO: add large/medium */
+    .container {
+        border: 1px var(--c-lg1) solid;
+        background-color: white;
+        box-shadow: 0px 1px 4px 0px #10151B1F; /* cdg3, 12% */
+        width: fit-content;
+        padding-left: var(--p);
+        padding-right: var(--p);
+        border-radius: 6px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        column-gap: 16px;
+
+        padding-top: 2px;
+        padding-bottom: 2px;
+    }
+
+    .container * {
+        font-size: 16px;
+    }
+
+    button * {
+        transition: color var(--v-animation-delay) ease-in-out,
+                    filter var(--v-animation-delay) ease-in-out;
+    }
+
+    button[data-selected=true] * {
+        font-weight: 500;
+        color: var(--c-a);
+    }
+
+    button[data-selected=true] img {
+        filter: invert(28%) sepia(92%) saturate(1649%) hue-rotate(194deg) brightness(99%) contrast(106%); /* accent */
+    }
+
+    button {
+        display: grid;
+        align-items: center;
+
+        text-align: center;
+
+        cursor: pointer;
+
+        color: var(--c-dg1);
+        fill: var(--c-dg1);
+    }
+
+    button img {
+        filter: invert(30%) sepia(7%) saturate(500%) hue-rotate(164deg) brightness(94%) contrast(89%); /* dg1 */
+    }
+
+    button:hover {
+        color: var(--c-dg2);
+    }
+
+    button:hover img {
+        filter: invert(15%) sepia(4%) saturate(1968%) hue-rotate(169deg) brightness(93%) contrast(90%); /* dg2 */
+    }
+
+    button[data-selected=true]:hover img {
+        filter: invert(28%) sepia(92%) saturate(1649%) hue-rotate(194deg) brightness(99%) contrast(106%); /* force accent if already accent */
+    }
+
+
+</style>
+
--- src/routes/(admin)/dashboard/meetups/[id]/edit/users/add/+page.svelte
+++ src/routes/(admin)/dashboard/meetups/[id]/edit/users/add/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import Breadcrumb from '$lib/components/global/Breadcrumb.svelte';
-    import Button from '$lib/components/global/Button.svelte';
+    import MultiSelect, { LabelType } from '$lib/components/global/MultiSelect.svelte';
+    import Form from '$lib/components/global/Form.svelte';
+
+    import puzzles from "$lib/data/puzzles";
+
+    let selectedEvents = new Set();
 
     import type { PageData } from './$types';
     export let data: PageData;
@@ -14,3 +19,17 @@
 ]} />
 
 <div style:height="16px" />
+
+
+<Form name="Add Competitor" fillWidth={false}>
+    <label class="form-label">
+        Add Events
+
+        <MultiSelect 
+            bind:selectedIndices={selectedEvents} 
+            padding={4} 
+            fixedHeight={false} 
+            labels={ Object.values(puzzles).map(p => ({type: LabelType.Image, data: p.icon})) } />
+    </label>
+</Form>
+
