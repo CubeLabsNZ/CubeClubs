a151fc20432c0187ec5ee818a859f6a28fc660b2
Reagan Bohan <xbjfk.github@gmail.com>
2023-10-01 08:03:41 +0000

    table

--- src/lib/components/global/Table.svelte
+++ src/lib/components/global/Table.svelte
@@ -1,28 +1,133 @@
+<script lang="ts">
+    import type { region } from "$lib/db/enums";
+    import Medal from "$lib/components/user/Medal.svelte";
+    import { formatTime } from "$lib/utils";
+    import { regionToString } from "$lib/data/regions";
 
+    export let list: {
+        value: number,
+        solves: {time: number}[] | undefined,
+        user_id: number,
+        user_name: string,
+        user_region: region,
+    }[]
+    export let solveCount: number | undefined
+
+    export let displayRank: boolean
+    export let displayMedals: boolean
+    export let proceedNum: number = 0
+</script>
+
+<table>
+    <tr>
+        {#if displayRank}
+            <th class="tc-ranking" />
+        {/if}
+        <th class="tc-name">Name</th>
+        <th class="tc-average">Average</th>
+        <th class="tc-best">Best</th>
+        <th class="tc-region">Region</th>
+        <!-- TODO: depends on if ao5, mo3, single... -->
+        {#each Array(solveCount) as _, i}
+            <th class="tc-solves">{i + 1}</th>
+        {/each}
+    </tr>
+
+    {#each list as { value, solves, user_id, user_name, user_region }, rank}
+        <tr>
+            <!-- if final round -->
+            {#if displayMedals}
+                <!-- TODO: error checking? -->
+                {#if rank < 3 && value != Infinity}
+                    <td class="tc-ranking">
+                        <div style:float="right">
+                            <Medal place={rank} />
+                        </div>
+                    </td>
+                {:else}
+                    <td class="tc-ranking">
+                        <p style:margin-right="4px">{rank + 1}</p>
+                    </td>
+                {/if}
+            {:else}
+                <td
+                    class="tc-ranking"
+                    data-proceed={(rank < proceedNum) &&
+                        value != Infinity}
+                >
+                    {rank + 1}
+                </td>
+            {/if}
+
+            <!-- TODO: make link -->
+            <td class="tc-name">{user_name}</td>
+            <td class="tc-average">{formatTime(value)}</td>
+            <td class="tc-best">{formatTime(Math.min(...solves))}</td>
+            <td class="tc-region">{regionToString(user_region)}</td>
+
+            {#each solves as time}
+                <td class="tc-solves">{formatTime(time)}</td>
+            {/each}
+        </tr>
+    {/each}
+</table>
 
 <style>
+
+    /* TODO: if we ever do sass, this code is first to use the features */
+    /* Top */
     tr:nth-child(2) td {
-        border-top: solid 8px white;
+        padding-top: calc(8px + 5.75px);
+        clip-path: inset(8px 0 0 0);
     }
 
+    /* Bottom */
     tr:last-child td {
-        border-bottom: solid 8px white;
+        padding-bottom: calc(8px + 5.75px);
+        clip-path: inset(0 0 8px 0);
     }
 
-    td:first-child, th:first-child {
-        border-left: solid 6px white;
+    /* Left */
+    td:first-child {
+        padding-left: calc(6px + 8.75px);
+        clip-path: inset(0 0 0 8.75px round var(--v-border-radius-small) 0 0 var(--v-border-radius-small));
+    }
+    /* Right */
+    td:last-child {
+        padding-right: calc(6px + 8.75px);
+        clip-path: inset(0 8.75px 0 0 round 0 var(--v-border-radius-small) var(--v-border-radius-small) 0);
     }
 
-    td:last-child, th:last-child {
-        border-right: solid 6px white;
+    /* Top Left */
+    tr:nth-child(2) td:first-child {
+        clip-path: inset(8px 0 0 8.75px round var(--v-border-radius-small) 0 0 var(--v-border-radius-small));
+    }
+    /* Top Right */
+    tr:nth-child(2) td:last-child {
+        clip-path: inset(8px 8.75px 0 0 round 0 var(--v-border-radius-small) var(--v-border-radius-small) 0);
+    }
+    /* Bottom Right */
+    tr:last-child td:last-child {
+        clip-path: inset(0 8.75px 8px 0 round 0 var(--v-border-radius-small) var(--v-border-radius-small) 0);
+    }
+    /* Bottom Left */
+    tr:last-child td:first-child {
+        clip-path: inset(0 0 8px 8.75px round var(--v-border-radius-small) 0 0 var(--v-border-radius-small));
     }
 
-    td, th {
-        padding-top: 5px;
-        padding-bottom: 4px;
+    tr:nth-child(even) td {
+        background-color: var(--c-lgh);
     }
 
-    .tc-name, .tc-region, .tc-solves {
+    td,
+    th {
+        padding-top: 5.75px;
+        padding-bottom: 5.75px;
+    }
+
+    .tc-name,
+    .tc-region,
+    .tc-solves {
         text-align: left;
     }
 
@@ -30,7 +135,8 @@
         min-width: 200px;
     }
 
-    .tc-average, .tc-name {
+    .tc-average,
+    .tc-name {
         font-weight: 500;
     }
 
@@ -38,7 +144,9 @@
         color: var(--c-a);
     }
 
-    .tc-average, .tc-best, .tc-ranking {
+    .tc-average,
+    .tc-best,
+    .tc-ranking {
         text-align: right;
     }
 
@@ -46,7 +154,7 @@
         color: var(--c-dg1);
     }
 
-    .tc-ranking[data-proceed=true] {
+    .tc-ranking[data-proceed="true"] {
         color: var(--c-green);
         font-weight: 600;
     }
--- src/routes/(app)/meetups/[id]/results/[roundId]/+page.svelte
+++ src/routes/(app)/meetups/[id]/results/[roundId]/+page.svelte
@@ -11,6 +11,7 @@
     import TabBar from '$lib/components/global/TabBar.svelte';
     import Breadcrumb from '$lib/components/global/Breadcrumb.svelte';
     import Medal, { PodiumPlace } from '$lib/components/user/Medal.svelte';
+    import Table from '$lib/components/global/Table.svelte';
     export let data;
 
     let locations = ["info", "competitors", "results"]
@@ -45,56 +46,14 @@
     {#if currentRound.results.length}
         {@const firstres = currentRound.results[0]}
         {@debug firstres}
-        <table>
-            <tr>
 
-                <th class="tc-ranking"></th>
-                <th class="tc-name">Name</th>
-                <th class="tc-average">Average</th>
-                <th class="tc-best">Best</th>
-                <th class="tc-region">Region</th>
-                <!-- TODO: depends on if ao5, mo3, single... -->
-                {#each Array(formats[currentRound.format].count) as _, i}
-                    <th class="tc-solves">{i + 1}</th>
-                {/each}
-
-            </tr>
-
-
-            {#each currentRound.results as { value, solves, user_id, user_name, user_region }, rank}
-                <tr>
-
-                    <!-- if final round -->
-                    {#if currentRound.number === data.maxRounds[currentRound.puzzle]}
-                        <!-- TODO: error checking? -->
-                        {#if rank < 3 && value != Infinity}
-                            <td class="tc-ranking">
-                                <div style:float=right>
-                                    <Medal place={rank} /> 
-                                </div>
-                            </td>
-                        {:else}
-                            <td class="tc-ranking">
-                                <p style:margin-right=4px> {rank + 1} </p> 
-                            </td>
-                        {/if}
-                    {:else}
-                        <td class="tc-ranking" data-proceed={(rank < currentRound.proceed_number ?? 0) && value != Infinity}> {rank + 1} </td>
-                    {/if}
-
-                    <!-- TODO: make link -->
-                    <td class="tc-name">{user_name}</td>
-                    <td class="tc-average">{formatTime(value)}</td>
-                    <td class="tc-best">{formatTime(Math.min(...solves))}</td>
-                    <td class="tc-region">{regionToString(user_region)}</td>
-
-                    {#each solves as time}
-                        <td class="tc-solves">{formatTime(time)}</td>
-                    {/each}
-
-                </tr>
-            {/each}
-        </table>
+        <Table
+                list={currentRound.results}
+                solveCount={formats[currentRound.format].count}
+                displayRank={true}
+                displayMedals={currentRound.number === data.maxRounds[currentRound.puzzle]}
+                proceedNum={currentRound.proceed_number}
+            />
     {:else}
         <p>No results yet - check back later!</p>
     {/if}
@@ -103,56 +62,5 @@
 <style>
     .content {
         margin-top: 16px;
-    }
-
-    /* Use border instead of padding: padding will use background color */
-    tr:nth-child(2) td {
-        border-top: solid 8px white;
-    }
-
-    tr:last-child td {
-        border-bottom: solid 8px white;
-    }
-
-    td:first-child, th:first-child {
-        border-left: solid 6px white;
-    }
-
-    td:last-child, th:last-child {
-        border-right: solid 6px white;
-    }
-
-    td, th {
-        padding-top: 5px;
-        padding-bottom: 4px;
-    }
-
-    .tc-name, .tc-region, .tc-solves {
-        text-align: left;
-    }
-
-    .tc-name {
-        min-width: 200px;
-    }
-
-    .tc-average, .tc-name {
-        font-weight: 500;
-    }
-
-    tr:not(:first-child) .tc-name {
-        color: var(--c-a);
-    }
-
-    .tc-average, .tc-best, .tc-ranking {
-        text-align: right;
-    }
-
-    .tc-ranking {
-        color: var(--c-dg1);
-    }
-
-    .tc-ranking[data-proceed=true] {
-        color: var(--c-green);
-        font-weight: 600;
     }
 </style>
