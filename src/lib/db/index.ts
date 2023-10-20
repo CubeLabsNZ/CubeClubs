0dc858cd85e5e590126e46cffaf3d83196f0103d
Reagan Bohan <xbjfk.github@gmail.com>
2023-10-20 22:28:31 +0000

    update packages

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -19,11 +19,6 @@ const dialect = new PostgresDialect({
 // to communicate with your database.
 export const db = new Kysely<DB>({
   dialect,
-  log(event) {
-    if (event.level === 'query') {
-      console.log(event.query.sql)
-      console.log(event.query.parameters)
-    }
-  }
+  log: ['query']
 })
 
