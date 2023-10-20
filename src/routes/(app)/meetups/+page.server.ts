a523751aed6854ef1595e0a422f5d256f95ff15c
Reagan Bohan <xbjfk.github@gmail.com>
2023-10-20 23:27:44 +0000

    fix meetup ordering

--- src/routes/(app)/meetups/+page.server.ts
+++ src/routes/(app)/meetups/+page.server.ts
@@ -18,14 +18,16 @@ export const load = (async () => {
             'club.name as club_name',
             'meetup.date'
         ])
-        .orderBy('date asc')
+        .orderBy('date desc')
         .groupBy(['meetup.id', 'club.name'])
     )
     .selectFrom('all_meetups')
     .select( eb => [
-        eb.fn.coalesce(eb.fn.agg('json_agg', ['all_meetups']).filterWhere('all_meetups.date', '<', new Date()), sql`'[]'`).as('past'),
+        //eb.fn.coalesce(eb.fn.agg('json_agg', ['all_meetups']).filterWhere('all_meetups.date', '<', new Date()), sql`'[]'`).as('past'),
+        sql<{}[]>`coalesce(json_agg("all_meetups" ORDER BY date DESC) filter(where "all_meetups"."date" < ${sql.lit(new Date())}), '[]')`.as('past'),
         eb.fn.coalesce(eb.fn.agg('json_agg', ['all_meetups']).filterWhere('all_meetups.date', '=', new Date()), sql`'[]'`).as('ongoing'),
-        eb.fn.coalesce(eb.fn.agg('json_agg', ['all_meetups']).filterWhere('all_meetups.date', '>', new Date()), sql`'[]'`).as('upcoming'),
+        //eb.fn.coalesce(eb.fn.agg('json_agg', ['all_meetups']).filterWhere('all_meetups.date', '>', new Date()), sql`'[]'`).as('upcoming'),
+        sql<{}[]>`coalesce(json_agg("all_meetups" ORDER BY date ASC) filter(where "all_meetups"."date" > ${sql.lit(new Date())}), '[]')`.as('upcoming')
     ])
     .executeTakeFirst()
     return meetups;
